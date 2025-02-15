7ab00366a709724c0e5327843f54ecd748db86ce
dcloud347 <17864201683@163.com>
2025-02-15 15:47:57 +0000

    feat: update confirm questions endpoint to handle JSON input and output

--- app/api/routes/config.py
+++ app/api/routes/config.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from fastapi import APIRouter, UploadFile, File
@@ -9,24 +10,12 @@ from app.models.resultModel import ResultModel
 router = APIRouter(prefix="/config", tags=["config"])
 
 
-# Confirm Questions by CSV file
-@router.post("/confirm-by-csv/", response_model=ResultModel)
-def confirm_by_csv(file: UploadFile = File(...)):
-    if not file.content_type.startswith("text/csv"):
-        return JSONResponse(content={"message": "Unsupported Media Type", "data":None}, status_code=400)
-    # save the file
-    with open("static/output.csv", "wb") as f:
-        f.write(file.file.read())
-    return ResultModel(message="success", data=None)
-
-@router.post("/confirm-by-json/")
+# Confirm quiz questions
+@router.post("/confirm-questions/")
 def confirm_by_json(data: QuestionsModel):
-    # turn the data into a csv file
-    csvHeader = ['question', 'choiceA', 'choiceB', 'choiceC', 'choiceD', 'answer']
-    file = open("static/output.csv", "w")
-    file.write(",".join(csvHeader)+'\n')
-    for question in data.questions:
-        file.write(",".join(question.__dict__.values())+'\n')
+    # turn the data into a json file
+    file = open("static/output.json", "w")
+    file.write(json.dumps(data, indent=4))
     file.close()
     return ResultModel(message="success", data=None)
 
@@ -44,9 +33,9 @@ def confirm_background(file: UploadFile = File(...)):
 @router.get("/get-questions/")
 def get_questions():
     # check if the file exists
-    if not os.path.exists("static/output.csv"):
+    if not os.path.exists("static/output.json"):
         return JSONResponse(content={"message": "No questions set", "data":None}, status_code=404)
-    return FileResponse("static/output.csv", media_type="text/csv", filename="QuizQuestions.csv")
+    return FileResponse("static/output.json", media_type="application/json", filename="quizQuestions.json")
 
 
 @router.get("/get-background-image/")
