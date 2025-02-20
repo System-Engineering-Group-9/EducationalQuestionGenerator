867bce65b4760f36d3c2c4ba021a1bc5c02728f5
dcloud347 <17864201683@163.com>
2025-02-20 16:01:39 +0000

    feat: update question generation to use Llama model and refactor prompt creation

--- app/ai/questionGenerator.py
+++ app/ai/questionGenerator.py
@@ -1,5 +1,4 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from llama_cpp import Llama
 
 from app.ai.enums.topic import Topic
 
@@ -27,70 +26,70 @@ class QuizQuestion:
                 f"Answer: {self.answer}")
 
 
+def get_prompt(number: int, topic: Topic, ageGroup: str, item: str = None) -> str:
+    if item is None:
+        prompt = (f"Generate {number} multiple choice quiz questions with 4 choices based on {topic.value},"
+                  f" suitable for children at {ageGroup} years old "
+                  f"and ensure there is only 1 correct answer, "
+                  f"include correct answer of multiple choice quiz question."
+                  f"Do not include any quotation marks in the output."
+                  f"Output each question in the format: "
+                  f"Question:"
+                  f"ChoiceA:"
+                  f"ChoiceB:"
+                  f"ChoiceC:"
+                  f"ChoiceD:"
+                  f"Answer:"
+                  f"Here is an example of output:"
+                  f"Question:What is the only planet that hosts life that we know of?\n"
+                  f"ChoiceA:Earth\n"
+                  f"ChoiceB:Mars\n"
+                  f"ChoiceC:Jupiter\n"
+                  f"ChoiceD:Venus\n"
+                  f"Answer:A"
+                  )
+    else:
+        prompt = (
+            f"Generate {number} multiple choice quiz questions around {item} with 4 choices based on {topic.value},"
+            f" suitable for children at {ageGroup} years old "
+            f"and ensure there is only 1 correct answer, "
+            f"include correct answer of multiple choice quiz question."
+            f"Do not include any quotation marks in the output."
+            f"Output each question in the format: "
+            f"Question:"
+            f"ChoiceA:"
+            f"ChoiceB:"
+            f"ChoiceC:"
+            f"ChoiceD:"
+            f"Answer:"
+            f"Here is an example of output:"
+            f"Question:What is the only planet that hosts life that we know of?\n"
+            f"ChoiceA:Earth\n"
+            f"ChoiceB:Mars\n"
+            f"ChoiceC:Jupiter\n"
+            f"ChoiceD:Venus\n"
+            f"Answer:A"
+            )
+    return prompt
+
+
 class QuestionGenerator:
     def __init__(self):
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        model_path = "ibm-granite/granite-3.1-2b-instruct"
-        # Load tokenizer
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        # Load Model
-        self.model = AutoModelForCausalLM.from_pretrained(model_path).to(self.device)
+        self.llm = Llama.from_pretrained(
+            repo_id="QuantFactory/granite-3.0-8b-instruct-GGUF",
+            filename="granite-3.0-8b-instruct.Q4_K_S.gguf",
+        )
 
     def generateQuestions(self, number: int, topic: Topic, ageGroup: str, item: str = None) -> list:
-        if item is None:
-            prompt = (f"Generate {number} multiple choice quiz questions with 4 choices based on {topic.value},"
-                      f" suitable for children at {ageGroup} years old "
-                      f"and ensure there is only 1 correct answer, "
-                      f"include correct answer of multiple choice quiz question."
-                      f"Do not include any quotation marks in the output."
-                      f"Output each question in the format: "
-                      f"Question:"
-                      f"ChoiceA:"
-                      f"ChoiceB:"
-                      f"ChoiceC:"
-                      f"ChoiceD:"
-                      f"Answer:"
-                      f"Here is an example of output:"
-                      f"Question:What is the only planet that hosts life that we know of?\n"
-                      f"ChoiceA:Earth\n"
-                      f"ChoiceB:Mars\n"
-                      f"ChoiceC:Jupiter\n"
-                      f"ChoiceD:Venus\n"
-                      f"Answer:A"
-                    )
-        else:
-            prompt = (f"Generate {number} multiple choice quiz questions around {item} with 4 choices based on {topic.value},"
-                    f" suitable for children at {ageGroup} years old "
-                    f"and ensure there is only 1 correct answer, "
-                    f"include correct answer of multiple choice quiz question."
-                    f"Do not include any quotation marks in the output."
-                      f"Output each question in the format: "
-                      f"Question:"
-                      f"ChoiceA:"
-                      f"ChoiceB:"
-                      f"ChoiceC:"
-                      f"ChoiceD:"
-                      f"Answer:"
-                    f"Here is an example of output:"
-                    f"Question:What is the only planet that hosts life that we know of?\n"
-                    f"ChoiceA:Earth\n"
-                    f"ChoiceB:Mars\n"
-                    f"ChoiceC:Jupiter\n"
-                    f"ChoiceD:Venus\n"
-                    f"Answer:A"
-            )
-
+        prompt = get_prompt(number, topic, ageGroup, item)
         chat = [
             {"role": "user", "content": prompt},
         ]
-        chat = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
-        input_tokens = self.tokenizer(chat, return_tensors="pt").to(self.device)
-        output = self.model.generate(**input_tokens, max_new_tokens=number * 100 + 200, temperature=0.85,
-                                     do_sample=True)
-        output = self.tokenizer.batch_decode(output, skip_special_tokens=True)[0]
-        assistantIndex = output.index("\nassistant")
-        text = output[assistantIndex + 10:]
-        textList = text.splitlines()
+        response = self.llm.create_chat_completion(
+            messages=chat
+        )
+        output = response['choices'][0]['message']['content']
+        textList = output.splitlines()
         textList = [text.strip() for text in textList if text != '']
         questions = []
         for index in range(0, len(textList), 6):
