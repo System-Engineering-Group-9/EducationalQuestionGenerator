32fbd354010d0e78883cc7b56288f05d0a282847
dcloud347 <17864201683@163.com>
2025-03-11 10:09:50 +0000

    feat: rename DEFAULT to NORMAL in QuizMode enum for clarity

--- app/models/enums/quizMode.py
+++ app/models/enums/quizMode.py
@@ -4,5 +4,6 @@ from enum import Enum
 class QuizMode(Enum):
     """Enum for the different quiz modes in the game"""
 
-    DEFAULT = 'DEFAULT'
+    NORMAL = 'NORMAL'
     BUZZ = 'BUZZ'
+    TIME_RUSH = 'TIME_RUSH'
